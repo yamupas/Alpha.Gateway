6e6c7ed7b52e63b7639f9dee48f9b8222bc9df72
YULIAN MURILLO <yamurillo@newsoft.com.co>
2023-08-10 21:42:04 -0500

    Update yaml and configuration

---
+++ Alpha.ApiGateway/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alpha.ApiGateway.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class DefaultController : ControllerBase
+    {
+        public DefaultController()
+        {
+
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Running");
+        }
+    }
+}
